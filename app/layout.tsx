9b2e5233d2b466d361fbe0f0440a156d9d003b14
naveedahmeds1 <na0953237@gmail.com>
2026-09-19 10:36:08 +0500

    Remove globals.css and add Tailwind CSS script
    Removed global CSS import and added Tailwind CSS script.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Special Note",
@@ -13,9 +12,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body className="antialiased bg-slate-950 text-white">
         {children}
       </body>
     </html>
   );
-  }
+}
